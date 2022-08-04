5079ca7f920976f44dbb3938bf8dbca18215335e
Pascal <dev.fullstack.js0311@gmail.com>
2022-08-04 12:48:32 +0300

    feat/add-new page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-
 import React from "react";
 import "./styles/App.css";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 import Menu from "./pages/Menu";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
-    <React.Fragment>
-      <Menu/>
-    </React.Fragment>
+    <BrowserRouter>
+     
+        <Routes>
+          <Route path="/" exact component={<Home/>} />
+          <Route path="/About" exact component={<About/>} />
+          <Route path="/Contact" exact component={<Contact/>} />
+          <Route path="/Menu" exact component={<Menu/>} />
+          <Route component={NotFound} />
+        </Routes>
+     
+
+    </BrowserRouter>
   );
 }
 
--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -2,9 +2,8 @@ import React from 'react';
 import '../styles/Menu.css';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
-import { AccessAlarm, ThreeDRotation } from '@mui/icons-material';
+import { NavLink } from 'react-router-dom';
 
-//import '../pages/image2.png';
 
 
 
@@ -12,24 +11,31 @@ const Menu = () => {
     return (
 
         <>
-
-            <div class="container-fluid">
+        <NavLink/>
+            <div className="container-fluid">
 
                 <header className='menu-header'>
-                    <h1 className='Tittle' >View Our <br /> New Menu</h1>
-                    <p className='paragraph' >The freshest ingredien for you every day</p>
+                    <ul>
+                        <li className='navbar1-left'>FoodZero</li>
+                        <li>M</li>
+                        <li className='navbar2-right' ><button id='navbar2-right1' >   reservations   </button></li>
+                        <li className='navbar2-right'>+8913548912</li>
+
+                    </ul>
+                    <h1 className='Tittle-left' id='Tittle' >View Our <br /> New Menu</h1>
+                    <p className='Tittle-left' id='paragraph' >The freshest ingredien for you every day</p>
                 </header>
                 <section className='section2'>
 
-                    <div class="row">
+                    <div className="row">
 
-                        <div class="col-6">
+                        <div className="col-6">
 
                             Première colonne
                             <img src="{}" alt='' />
                         </div>
 
-                        <div class="col-6">
+                        <div className="col-6">
 
                             Deuxième colonne
 
@@ -44,21 +50,21 @@ const Menu = () => {
                 <section className='section3 '>
                     <p className='section3-tittle1'>Make a reservation</p>
                     <h6 className='section3-tittle2'>Get in touch with restaurant</h6>
-                    <div id='box-section' class="row">
+                    <div id='box-section' className="row">
 
-                        <div class="col"><TextField id="outlined-basic" label="16/22/2021" variant="outlined" /></div>
+                        <div className="col"><TextField id="outlined-basic" label="16/22/2021" variant="outlined" /></div>
 
-                        <div class="col"> <TextField
+                        <div className="col"> <TextField
                             id="outlined-select-currency"
-                            select
                             label="6:00 pm"
+                            select
                             helperText="Please select your currency"
                         ></TextField></div>
 
-                        <div class="col"><TextField
+                        <div className="col"><TextField
                             id="outlined-select-currency"
-                            select
                             label="6:00 pm"
+                            select
                             helperText="Please select your currency"
                         ></TextField></div>
                     </div>
@@ -77,16 +83,16 @@ const Menu = () => {
 
                             <div id='left-footer' className="col">food <br />zero.</div>
 
-                            <div id='left2-footer' className="col">Contact<br/>+36145545555<br/>foodzer@mail.fr<br/>melville.rue valence<br/>BP:1226 np,sharll</div>
+                            <div id='left2-footer' className="col">Contact<br />+36145545555<br />foodzer@mail.fr<br />melville.rue valence<br />BP:1226 np,sharll</div>
 
                             <div id='left4-footer' className="col"><Button variant="outlined">Subcribe</Button></div>
 
                         </div>
                         <div className="row align-items-left">
 
-                            <div class="col"></div>
+                            <div className="col"></div>
 
-                            <div id='left3-footer' class="col">Never miss a recipe<br/><TextField id="outlined-basic" label=" E-mail adress " variant="outlined" /></div>
+                            <div id='left3-footer' className="col">Never miss a recipe<br /><TextField id="outlined-basic" label=" E-mail adress " variant="outlined" /></div>
 
                         </div>
 
@@ -107,7 +113,7 @@ const Menu = () => {
 
                         </div>
 
-                    </div>---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------<br/>
+                    </div>--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------<br />
                     <p>Zero Inc,All rigths Reserved</p>
                 </footer>
             </div>
